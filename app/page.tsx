2baa9dc592aaf110e401a7385388c4cfc1793bbc
hakimleon <hakimleon@gmail.com>
2023-10-28 16:23:03 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 import AcmeLogo from '@/app/ui/acme-logo';
+import Image from 'next/image';
 
 import Link from 'next/link';
+import { lusitana } from './ui/fonts';
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+    <main className="flex min-h-screen flex-col p-2">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-24">
+        <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-100 px-6 py-10 md:w-2/5 md:px-20">
+          <p className={`text-lg text-gray-800 md:text-xl md:leading-normal ${lusitana.className}`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
@@ -18,15 +20,17 @@ export default function Page() {
           </p>
           <Link
             href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            className="self-start font-semibold rounded-md bg-blue-500 px-6 py-2 text-sm shadow-xl hover:shadow-md  text-white transition-all hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span>
           </Link>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+        <div className="flex items-center justify-center p-2 md:w-3/5 md:px-2 md:py-4">
           {/* Add Hero Images Here */}
+          <Image src='/hero-desktop.png' width={700} height={320} alt='hero image' className='hidden md:block' />
+          <Image src='/hero-mobile.png' width={560} height={620} alt='hero image' className='md:hidden block' />
         </div>
       </div>
-    </main>
+    </main> 
   );
 }
